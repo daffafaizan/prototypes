12bceae69bc9c560934ad60a7335a4d07187b85d
Daffa <daffamfzn@gmail.com>
2025-02-10 00:46:48 +0700

    feat: pay-it-forward

---
+++ packages/contracts/src/Chain.sol
@@ -0,0 +1,132 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.13;
+
+// Solution to check if chain exists: https://ethereum.stackexchange.com/questions/13021/how-can-i-figure-out-if-a-certain-key-exists-in-a-mapping-struct-defined-inside
+
+contract ChainContract {
+    // sets of the contract and saves competition manager address
+    address manager;
+
+    constructor() {
+        manager = msg.sender;
+    }
+
+    //modifier to determine if function call is coming from competition manager
+    modifier isCompManager() {
+        require(msg.sender == manager, "The manager must call this.");
+        _;
+    }
+
+    // struct representing a pay-it-forward chain
+    // we record who participated, how many times, and the total number of transactions
+    struct Chain {
+        suint chainId; // unique id for chain
+        mapping(saddress => suint) links; // participant/business => # of times they paid into the chain
+        suint uniqueBusinesses; // # of different businesses in the chain
+        suint uniqueParticipants; // # of different participants in the chain
+        suint chainLength; // total length of the chain
+    }
+
+    // struct representing the records of all nuked chains accumulated over the course of a competition
+    // we keep track of the final score and length of the nuked chains, and the overall longest chain
+    struct CompStats {
+        mapping(suint => suint) chainFinalScores; // nuked chain's ID => that chain's final score
+        mapping(suint => suint) chainFinalLengths; // nuked chain's ID => that chain's total length
+        suint overallWinningChainId; // chainId of the overall winning chain
+    }
+
+    // struct representing the records of each participant and business specific chain details accumulated over the course of a competition
+    // we record the last chain they paid into, the longest chain they paid into, and how many times they paid into the longest chain
+    struct UserStats {
+        mapping(saddress => suint) latestChain; // participant/business => latest chain's ID
+        mapping(saddress => suint) bestChain; // participant/business => longest chain's ID
+        mapping(saddress => suint) bestChainLinks; // participant/business => longest chain's # of links
+    }
+
+    Chain activeChain; // current running chain
+    CompStats comp; // records of all comp global data
+    UserStats user; // records all user specific data
+
+    // adds a link to the active chain
+    function forgeLink(saddress pAddr, saddress bAddr) public isCompManager {
+        // check if participant is paying into the active chain for the first time
+        // if so, update their personal stats and increment # of different people in the active chain
+        if (user.latestChain[pAddr] != activeChain.chainId) {
+            updateToCurrentChain(pAddr);
+            activeChain.uniqueParticipants++;
+        }
+        activeChain.links[pAddr]++; // increment the participants # of links in active chain
+
+        // check if business is being added to the active chain for the first time
+        // if so, update their personal stats and increment # of different businesses in the active chain
+        if (user.latestChain[bAddr] != activeChain.chainId) {
+            updateToCurrentChain(bAddr);
+            activeChain.uniqueBusinesses++;
+        }
+        activeChain.links[bAddr]++; // increment the businesses # of links in active chain
+    }
+
+    // helper function to reset the participant/business links and assign their latest chain to the active chain
+    function updateToCurrentChain(saddress addr) internal {
+        checkIsChainLongest(addr); // check if last chain is participant/business longest chain
+        activeChain.links[addr] = suint(0);
+        user.latestChain[addr] = activeChain.chainId;
+    }
+
+    // check if participant/business latest chains score is greater than their current highest chain score. If so, overwrite it
+    function checkIsChainLongest(saddress addr) public {
+        if (comp.chainFinalScores[user.latestChain[addr]] > comp.chainFinalScores[user.bestChain[addr]]) {
+            user.bestChain[addr] = user.latestChain[addr]; // update the longest chain to the new longest chain
+            user.bestChainLinks[addr] = activeChain.links[addr]; // update longest chain count to the new count
+        }
+    }
+
+    // saves the necessary stats of the chain being nuked, updates the overall winning chain, and resets the active chain
+    function nuke() public isCompManager {
+        // save the final score and length of the nuked chain
+        comp.chainFinalScores[activeChain.chainId] =
+            calcChainScore(activeChain.uniqueParticipants, activeChain.uniqueBusinesses);
+        comp.chainFinalLengths[activeChain.chainId] = activeChain.chainLength;
+        // compare scores for nuked chain with overall winning chain
+        if (comp.chainFinalScores[activeChain.chainId] > comp.chainFinalScores[comp.overallWinningChainId]) {
+            comp.overallWinningChainId = activeChain.chainId;
+        }
+        //reset the stats of the active chain
+        activeChain.chainLength = suint(0);
+        activeChain.uniqueParticipants = suint(0);
+        activeChain.uniqueBusinesses = suint(0);
+        activeChain.chainId++;
+    }
+
+    // helper function to calculate a chains' final score
+    function calcChainScore(suint pScore, suint bScore) internal pure returns (suint) {
+        // the equation is finalScore = # unique businesses * [1 + (#unique participants/10)]
+        return bScore * (suint256(1) + (pScore / suint256(10)));
+    }
+
+    // getter function for winning chain
+    function getWinningChainId() public view isCompManager returns (uint256) {
+        return uint256(comp.overallWinningChainId);
+    }
+
+    // getter function for the winning chains total length
+    function getWinningChainLength() public view isCompManager returns (uint256) {
+        return uint256(comp.chainFinalLengths[comp.overallWinningChainId]);
+    }
+
+    // getter function for participant/business best chain
+    function getBestChainId(saddress addr) public view isCompManager returns (uint256) {
+        return uint256(user.bestChain[addr]);
+    }
+
+    // getter function for participant/business best chains count
+    function getBestChainCount(saddress addr) public view isCompManager returns (uint256) {
+        return uint256(user.bestChainLinks[addr]);
+    }
+
+    // setter function for when a participant is claims payout, so they can't claim repeatedly
+    function walletHasBeenPaid(saddress addr) public isCompManager {
+        user.bestChainLinks[addr] = suint(0);
+    }
+}
---
+++ packages/contracts/src/CompManager.sol
@@ -0,0 +1,231 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.13;
+
+import "./Chain.sol";
+import "./IStoreFront.sol";
+
+contract CompManager {
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~at deploy~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // settings for the owner/manager and creating the chainContract and its address
+    address public manager;
+    ChainContract public chain;
+    address chainAddress;
+    // address of charity selected for competition
+    address charity;
+    mapping(address => bool) approvedBusinesses;
+    // setting up the competititon phase cycles
+    CompetitionPhases public competition = CompetitionPhases.PRE;
+
+    // constructor function that run at deployment
+    constructor(address charAddr) {
+        require(charAddr != address(0x0), "Invalid charity address.");
+        manager = msg.sender;
+        chain = new ChainContract();
+        chainAddress = address(chain);
+        charity = charAddr;
+    }
+
+    // enum sets up the different phases of the game, which loop
+    enum CompetitionPhases {
+        PRE,
+        DURING,
+        POST
+    }
+
+    // modifier to restrict function calls to just the manager of the contract
+    modifier managerOnly() {
+        require(msg.sender == manager, "The competition manager must call this.");
+        _;
+    }
+
+    // modifier to restrict function calls to only calls that contain an approved business wallet
+    modifier approvedBusinessesOnly(address businessAddr) {
+        require(approvedBusinesses[businessAddr], "This business is not approved.");
+        _;
+    }
+
+    // modifier to restrict function calls to only if they occur during the correct stage
+    modifier atStage(CompetitionPhases phase) {
+        require(phase == competition, "This function is is invalid at this stage.");
+        _;
+    }
+
+    // public vars are the vars we want people to read/verify the rules of the competition that we laid out
+    // the percentages of the prize pot each group has access to
+    uint256 public constant participantPerc = 50;
+    uint256 public constant businessPerc = 15;
+    uint256 public constant charityPerc = 35;
+    // the prize pot allotment val (money going to pot per transaction)
+    uint256 public constant prizePotAllotment = 1;
+    // competition time parameters
+    uint256 public competitionStartTime;
+    uint256 public duration = 10; //this should be read as seconds, i.e. 2592000 seconds = 30 days
+
+    // amount being donated to charity
+    uint256 donation;
+    // vals used to calculate the single entry value of participant/business winnings
+    uint256 pWinnings;
+    uint256 bWinnings;
+
+    // important flag/db for competition
+    sbool lastPifChoice = sbool(false);
+
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~pre competition~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // functions to set up the competition prior to starting it
+
+    // determines the charity we will be donating to
+    function selectCharity(address charityAddr) public managerOnly atStage(CompetitionPhases.PRE) {
+        require(charityAddr != address(0x0), "Invalid address.");
+        charity = charityAddr;
+    }
+
+    // a business approval process so that only businesses that we have agreed with can be involved in the competition
+    // safeguards against random people setting up wallets as fake businesses
+    function businessApprovalProcess(address businessAddr) public managerOnly atStage(CompetitionPhases.PRE) {
+        approvedBusinesses[businessAddr] = true;
+    }
+
+    // If any business if feeling generous and wants to donate money to the prize pool
+    function businessContribution()
+        external
+        payable
+        approvedBusinessesOnly(msg.sender)
+        atStage(CompetitionPhases.PRE)
+    {
+        require(msg.value > 0, "Contribution has been rejected.");
+    }
+
+    // starts the competition
+    function startCompetition() public managerOnly atStage(CompetitionPhases.PRE) {
+        competitionStartTime = block.timestamp;
+        competition = CompetitionPhases.DURING;
+    }
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~during competition~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // Called by competition customers when they want to purchase things
+    // currentPifChoice: flag determining if you chose to PIF
+    // prodID: indicates what you want to buy, this is part of the Storefront interface that we assume all businesses have implemented
+
+    function makeCompTransaction(sbool currentPifChoice, address business, suint prodID)
+        external
+        payable
+        approvedBusinessesOnly(business)
+        atStage(CompetitionPhases.DURING)
+    {
+        if (lastPifChoice && currentPifChoice) {
+            // case: last person pif, current person pif
+            // send money to business
+            uint256 prodPrice = msg.value - (5 + (prizePotAllotment / 100)); //subtract pif amount and prize pot amount
+            IStoreFront(business).purchase{value: prodPrice}(prodID); // store implements their own purchase logic
+
+            // rebate: yes, last person pif
+            (bool success,) = msg.sender.call{value: 5}("");
+            require(success, "Rebate failed.");
+
+            // update chain with new pif information
+            chain.forgeLink(saddress(msg.sender), saddress(business));
+
+            // don't update lastPifChoice because it is the same
+        } else if (!lastPifChoice && currentPifChoice) {
+            // case: last person did not pif, current person pif
+            // send money to business
+            uint256 prodPrice = msg.value - (5 + (prizePotAllotment / 100)); //subtract pif amount and prize pot amount
+            IStoreFront(business).purchase{value: prodPrice}(prodID); // store implements their own purchase logic
+
+            // rebate: no, last person didn't pif
+
+            // update chain with new pif information
+            chain.forgeLink(saddress(msg.sender), saddress(business));
+
+            //update lastPifChoice to new choice
+            lastPifChoice = currentPifChoice;
+        } else if (lastPifChoice && !currentPifChoice) {
+            // case: last person pif, current person did not pif
+            // send money to business
+            uint256 prodPrice = msg.value; // amount sent is exact
+            IStoreFront(business).purchase{value: prodPrice}(prodID); // store implements their own purchase logic
+
+            // rebate: yes, last person pif
+            (bool success,) = msg.sender.call{value: 5}("");
+            require(success, "Rebate failed.");
+
+            // nuke the existing chain
+            chain.nuke();
+
+            // update lastPifChoice to new choice
+            lastPifChoice = currentPifChoice;
+        } else {
+            // last person did not pif, you did not pif
+            // send money to business
+            uint256 prodPrice = msg.value; // since current person didn't pif, they send the exact amount
+            IStoreFront(business).purchase{value: prodPrice}(prodID); // store implements their own purchase logic
+
+            // rebate: no, last person didn't pif
+
+            // no update/nuke because chain is default
+
+            //don't update last PIF because it is the same
+        }
+        endCompetition(); //checks every transaction if the competition should be ended
+    }
+
+    // checks if the competition has been running for the set duration, and if so ends the competition
+    function endCompetition() internal atStage(CompetitionPhases.DURING) {
+        if (block.timestamp >= competitionStartTime + duration) {
+            competition = CompetitionPhases.POST;
+            setupPostCompetition();
+        }
+    }
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~post competition~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    // sets up post competition conditions
+
+    function setupPostCompetition() internal atStage(CompetitionPhases.POST) {
+        chain.nuke(); //performs final nuke to save final active chains stats and compare to the overall winning chain
+        setupPayout(); //determines the payouts
+    }
+
+    // helper function for setupPostCompetition
+    // determines amount to be paid per entry in the chain for participant/business
+    // the portion being donated to charity is calculated and then donated
+    function setupPayout() internal atStage(CompetitionPhases.POST) {
+        // calc prize pot amount of a single link for participant/business
+        pWinnings = ((address(this).balance * participantPerc) / (chain.getWinningChainLength() * 100));
+        bWinnings = ((address(this).balance * businessPerc) / (chain.getWinningChainLength() * 100));
+
+        // calc amount of prize pot being donated to charity
+        donation = ((address(this).balance * charityPerc) / 100);
+
+        // check if charity address if valid
+        require(charity != address(0x0), "Invalid address.");
+        // payout charity
+        (bool paid,) = charity.call{value: donation}("");
+        require(paid, "Donation failed.");
+    }
+
+    // payout function participants/businesses will execute to claim their respective payouts
+    function payout() external payable atStage(CompetitionPhases.POST) {
+        chain.checkIsChainLongest(saddress(msg.sender));
+        if (chain.getBestChainId(saddress(msg.sender)) == chain.getWinningChainId()) {
+            uint256 linkCount = chain.getBestChainCount(saddress(msg.sender)); // gets participant/business # of links
+            if (!approvedBusinesses[msg.sender]) {
+                // payout to participant
+                (bool paid,) = msg.sender.call{value: linkCount * pWinnings}("");
+                require(paid, "Payout failed.");
+            } else {
+                // payout to business
+                (bool paid,) = msg.sender.call{value: linkCount * bWinnings}("");
+                require(paid, "Payout failed.");
+            }
+            chain.walletHasBeenPaid(saddress(msg.sender)); // if payment goes through we reset # of links to prevent repeated payouts
+        }
+
+        // checks every payout if the competition can be reset
+        resetCompetition();
+    }
+
+    // reset's the competition if everyone has been paid out and the wallet balance is empty
+    function resetCompetition() internal atStage(CompetitionPhases.POST) {
+        if (address(this).balance == 0) {
+            competition = CompetitionPhases.PRE;
+        }
+    }
+}
